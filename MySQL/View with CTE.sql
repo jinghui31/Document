3224a2df0dc752fc66a4077ca6fc818e2b776fb6
Jason <jinghui31@gmail.com>
2019-03-13 16:21:04 +0800

    CTE sample

--- MySQL/View with CTE.sql
+++ MySQL/View with CTE.sql
@@ -1,9 +1,9 @@
 CREATE VIEW `machine_alarm` AS
 WITH alarm_data_info AS (
-	SELECT machine_id, alarm_id, start_date_time, end_date_time
+	SELECT machine_id, alarm_id, start_date_time
 	FROM alarm_data
 	WHERE end_date_time is NULL
-)	SELECT ROW_NUMBER() OVER w AS 'id', adi.machine_id, m.name AS 'machine_name', adi.alarm_id, a.name AS 'alarm_name', adi.start_date_time, adi.end_date_time
+)	SELECT ROW_NUMBER() OVER w AS 'id', adi.machine_id, m.name AS 'machine_name', a.name AS 'alarm_name', adi.start_date_time
 	FROM alarm_data_info adi 
 		LEFT JOIN machine m
 		ON adi.machine_id = m.id 
